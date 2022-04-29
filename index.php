788d58d2510e4b501beb0c15653be051376fc110
hailm <hai.lemanh.1310@gmail.com>
2022-04-29 15:28:04 +0700

    update

--- index.php
+++ index.php
@@ -1,5 +1,45 @@
 <?php $data["title"] = "Trang chủ"; ?>
 <?php $body = '' ?>
 <?php require "template-parts/layouts/header.php"; ?>
+<div class="home__block1 uk-section uk-background-norepeat uk-height-viewport" data-src="images/bg-travel-20220325111838.jpeg" uk-img>
+    <div class="uk-container">
+        <div uk-grid>
+            <div class="uk-width-2-5@m">
+                <img src="images/logo-20220325111935.png" alt="">
+            </div>
+        </div>
+    </div>
+</div>
+<div class="home__block2 uk-flex uk-flex-middle">
+    <div class="home__block2__imgCover uk-cover-container uk-position-top-right">
+        <img src="images/artboard-42x-20210416035838.png" alt="" uk-cover="">
+        <canvas width="910" height="500"></canvas>
+    </div>
+    <div class="uk-width-1-1 uk-section">
+        <div class="uk-container">
+            <div class="uk-margin uk-text-center">
+                <div class="uk-cover-container home__block2__cover1 uk-flex-inline">
+                    <img src="images/artboard-19-20210419034240.png" alt="" uk-cover="">
+                    <canvas width="1224" height="550"></canvas>
+                </div>
+            </div>
+            <div class="uk-margin-top">
+                <div class="uk-child-width-1-4@m" uk-grid>
+                    <?php
+                    $data = array(
+                        array(
+                            'src' => '',
+                            'txt' => '',
+                        ),
+                    );
+                    foreach ($data as $k=>$v): ?>
+                    <div>
 
+                    </div>
+                    <?php endforeach; ?>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 <?php require "template-parts/layouts/footer.php"; ?>
--- template-parts/layouts/footer.php
+++ template-parts/layouts/footer.php
@@ -1,3 +1,44 @@
+<div class="footer uk-position-relative">
+    <div class="uk-position-cover footer__bg" data-src="images/mo8-vfhb20220325111008.jpeg" uk-img></div>
+    <div class="uk-section uk-position-relative">
+        <div class="uk-container">
+            <div class="footer__item">
+                <div class="uk-flex-middle" uk-grid>
+                    <div class="uk-width-1-3@m uk-text-center">
+                        <div class="uk-cover-container uk-flex-inline">
+                            <img src="images/logo-accesstrade-3_150-1-20210325120138.png" alt="" uk-cover="">
+                            <!--<canvas width="675" height="450"></canvas>-->
+                            <canvas width="177" height="118"></canvas>
+                        </div>
+                    </div>
+                    <div class="uk-width-expand">
+                        <div class="uk-child-width-1-2@m" uk-grid>
+                            <div>
+                                <h3 class="uk-h3 footer__title">Trụ sở chính</h3>
+                                <ul class="uk-list footer__list">
+                                    <li class="address">Tầng 7, số 97-99 Láng Hạ, Phường Láng Hạ, Quận Đống Đa, Tp Hà Nội</li>
+                                    <li class="phone">028 3970 0359</li>
+                                    <li class="mail">support@accesstrade.vn</li>
+                                </ul>
+                            </div>
+                            <div>
+                                <h3 class="uk-h3 footer__title">VP Tp Hồ Chí Minh</h3>
+                                <ul class="uk-list footer__list">
+                                    <li class="address">Số 927/1, đường CMT8, Phường 7, Quận Tân Bình, Tp Hồ Chí Minh</li>
+                                    <li class="phone">028 3970 0359</li>
+                                    <li class="mail">support@accesstrade.vn</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="footer__item">
+                <div class="uk-text-center footer__copyright">© 2022 ACCESSTRADE a product of INTERSPACE VIETNAM. <br class="uk-visible@s"> All rights reserved.</div>
+            </div>
+        </div>
+    </div>
+</div>
 </div>
 <!--/app-->
 </body>
